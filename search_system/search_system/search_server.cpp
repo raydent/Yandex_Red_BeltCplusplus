f097e9a5c19eede77155a7d91533ee1d2e508da1
Raydent <benisgachi@gmail.com>
2020-03-25 04:23:50 +0300

    first part

--- search_system/search_system/search_server.cpp
+++ search_system/search_system/search_server.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <iostream>
 #include <cstring>
+#include <deque>
 vector<string> SplitIntoWords(const string& line) {
     istringstream words_input(line);
     return { istream_iterator<string>(words_input), istream_iterator<string>() };
@@ -25,30 +26,140 @@ void SearchServer::UpdateDocumentBase(istream& document_input) {
     index = move(new_index);
 }
 
+class TopFive {
+public:
+    void Add(size_t index, size_t i) {
+        if (size_ < 5) {
+            data[index].push_back(i);
+            size_++;
+            min = data.begin()->first;
+        }
+        else if (index > min) {
+            auto beg = data.begin();
+            beg->second.pop_back();
+            data[index].push_back(i);
+            if (beg->second.empty()) {
+                data.erase(data.begin());
+            }
+            min = data.begin()->first;
+        }
+    }
+    vector<pair<size_t, size_t>> get_data() {
+        //return data;
+        vector<pair<size_t, size_t>> res;
+        /*for (const auto& i : data) {
+            for (const auto& i1 : i.second) {
+                res.push_back({i1, i.first});
+            }
+        }*/
+        if (!data.empty()) {
+            auto it = end(data);
+            it--;
+            while (it != begin(data)) {
+                for (const auto& i : it->second) {
+                    res.push_back({ i, it->first });
+                }
+            }
+            for (const auto& i : it->second) {
+                res.push_back({ i, it->first });
+            }
+        }
+        return res;
+    }
+private:
+    size_t size_ = 0;
+    map<size_t, vector<size_t>> data;
+    size_t min = 0;
+    //bool overload = true;
+};
+
+bool comp(const pair<size_t, deque<size_t>>& lhs,
+    const pair<size_t, deque<size_t>>& rhs) {
+    return lhs.first < rhs.first;
+};
+class TestCont {
+public:
+    TestCont(): data(0), size_(0), min(0){};
+    void Add(size_t index, size_t i) {
+        deque<size_t> crutch;
+        if (size_ < 5) {
+            auto it = lower_bound(begin(data), end(data), make_pair(index, crutch), comp);
+            //data.insert(it, {});
+            if (it == end(data)) {
+                data.push_back({ index, {i} });
+            }
+            else {
+                if (it->first == index) {
+                    data[it - begin(data)].second.push_back(i);
+                }
+                else {
+                    data.insert(it, { index, {i} });
+                }
+            }
+            min = data[0].first;
+            size_++;
+        }
+        else if (index > min) {
+            auto it = lower_bound(begin(data), end(data), make_pair(index, crutch), comp);
+            if (it == end(data)) {
+                data.push_back({ index, {i} });
+            }
+            else {
+                if (it->first == index) {
+                    data[it - begin(data)].second.push_back(i);
+                }
+                else {
+                    data.insert(it, { index, {i} });
+                }
+            }
+            data[0].second.pop_back();
+            if (data[0].second.empty())
+                data.pop_front();
+            min = data[0].first;
+        }
+    };
+    vector<pair<size_t, size_t>> get_data() {
+        vector<pair<size_t, size_t>> res;
+        if (!data.empty()) {
+            auto it = end(data);
+            it--;
+            while (it != begin(data)) {
+                for (const auto& i : it->second) {
+                    res.push_back({ i, it->first });
+                }
+            }
+            for (const auto& i : it->second) {
+                res.push_back({ i, it->first });
+            }
+        }
+        return res;
+    }
+private:
+    deque<pair<size_t, deque<size_t>>> data;
+    size_t size_ = 0;
+    size_t min = 0;
+};
+
+
 void SearchServer::AddQueriesStream(
     istream& query_input, ostream& search_results_output
 ) {
-    vector<size_t> docid_count(__MAX_DOC_NUM__);
+    vector<pair<size_t, size_t>> docid_count(index.docs_size());
     for (string current_query; getline(query_input, current_query); ) {
-        const auto words = SplitIntoWords(current_query);
-        memset(&docid_count[0], 0, __MAX_DOC_NUM__);
-        //map<size_t, size_t> docid_count;
+        const auto words = SplitIntoWords(move(current_query));
+        for (size_t i = 0; i < docid_count.size(); i++) {
+            docid_count[i] = { i, 0 };
+        }
         for (const auto& word : words) {
-            for (const size_t docid : index.Lookup(word)) {//to change depending on lookup structure
-                docid_count[docid]++;
+            for (const auto& i : index.Lookup(word)) {
+                docid_count.at(i.first).second += i.second;
             }
         }
-
-        vector<pair<size_t, size_t>> search_results(__MAX_DOC_NUM__);
-        for (int i = 0; i < __MAX_DOC_NUM__; i++) {
-            search_results.push_back({i, docid_count[i]});
-        }
-        auto mid = search_results.size() > 5 ? begin(search_results) + 5 :
-            begin(search_results) + search_results.size();
+        auto mid = docid_count.size() > 5 ? docid_count.begin() + 5 : docid_count.end();
         partial_sort(
-            begin(search_results), 
+            begin(docid_count),
             mid,
-            end(search_results),
+            end(docid_count),
             [](pair<size_t, size_t> lhs, pair<size_t, size_t> rhs) {
                 int64_t lhs_docid = lhs.first;
                 auto lhs_hit_count = lhs.second;
@@ -57,14 +168,10 @@ void SearchServer::AddQueriesStream(
                 return make_pair(lhs_hit_count, -lhs_docid) > make_pair(rhs_hit_count, -rhs_docid);
             }
         );
-        int not_zero_index = 0;
-        while (not_zero_index != 5) {
-            if (search_results[not_zero_index].second == 0)
-                break;
-            not_zero_index++;
-        }
+
         search_results_output << current_query << ':';
-        for (auto [docid, hitcount] : Head(search_results, not_zero_index)) {
+        for (auto& [docid, hitcount] : Head(docid_count, 5)) {
+            if (hitcount == 0) break;
             search_results_output << " {"
                 << "docid: " << docid << ", "
                 << "hitcount: " << hitcount << '}';
@@ -78,15 +185,20 @@ void InvertedIndex::Add(const string& document) {
 
     const size_t docid = docs.size() - 1;
     for (const auto& word : SplitIntoWords(document)) {
-        index[word].push_back(docid);
+        auto& temp = index[word];
+        if (temp.empty() || temp.back().first != docid) {
+            temp.push_back({ docid, 0 });
+        }
+        temp.back().second++;
     }
 }
 
-list<size_t> InvertedIndex::Lookup(const string& word) const {
+const vector<pair<size_t, size_t>>& InvertedIndex::Lookup(const string& word) const {
+    vector<pair<size_t, size_t>> crutch;
     if (auto it = index.find(word); it != index.end()) {
         return it->second;
     }
     else {
-        return {};
+        return crutch;
     }
 }
--- search_system/search_system/search_server.h
+++ search_system/search_system/search_server.h
@@ -13,20 +13,23 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <unordered_map>
 using namespace std;
 
 #define __MAX_DOC_NUM__ 50000
 class InvertedIndex {
 public:
     void Add(const string& document);
-    list<size_t> Lookup(const string& word) const;
+    const vector<pair<size_t, size_t>>& Lookup(const string& word) const;
 
     const string& GetDocument(size_t id) const {
         return docs[id];
     }
-
+    const size_t docs_size() const {
+        return docs.size();
+    }
 private:
-    map<string, list<size_t>> index;
+    map<string, vector<pair<size_t, size_t>>> index;
     vector<string> docs;
 };
 
